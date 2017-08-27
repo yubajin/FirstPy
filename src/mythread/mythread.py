c5b2115137342c50ff34ac5c42a3342c85a371a4
yubajin <1803279085@qq.com>
2017-08-27 09:32:43 +0800

    增加了线程

---
+++ src/mythread/mythread.py
@@ -0,0 +1,17 @@
+import threading
+
+def thread_job():
+    print('This is an added Thread,number is %s' % threading.current_thread())
+
+
+def main():
+    add_thread = threading.Thread(target=thread_job)
+    add_thread.start()
+    print('threading active_count:',threading.active_count())#查看开通线程个数
+    print('threading.enumerate:', threading.enumerate());#查看开通哪几个线程
+    print('current_thread:',threading.current_thread())
+    print('main_thread:',threading.main_thread())
+    print('get_ident:',threading.get_ident)
+
+if  __name__  == '__main__':
+    main()
